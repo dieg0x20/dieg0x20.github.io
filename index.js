f7e20094396639954eb878f39d3737f2da4c005d
diego <diego444x@gmail.com>
2024-08-28 20:36:24 -0300

    update home page

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
+const url = "https://api.github.com/users/dieg0x20";
+const res = document.getElementById("profile");
+
+fetch(url)
+    .then(response => response.json())
+    .then(data => {
+        res.innerHTML += `
+            <img src="${data.avatar_url}" alt="avatar">
+            <h1>Diego_dev</h1>
+        `;
+    })
+    .catch(error => console.error('Error fetching data:', error));
+
 document.addEventListener('DOMContentLoaded', function () {
     const toggleMenuButton = document.querySelector('.toggle-menu');
     const navList = document.querySelector('.nav-list');
