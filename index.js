2b03f4c4b833e7e92a508c7549d1a907a3ca5497
Diego <diego444x@gmail.com>
2024-08-30 09:33:36 -0300

    Update index.js

--- index.js
+++ index.js
@@ -2,21 +2,51 @@ const url = "https://api.github.com/users/dieg0x20";
 const res = document.getElementById("profile");
 
 fetch(url)
-    .then(response => response.json())
-    .then(data => {
-        res.innerHTML += `
+  .then(response => response.json())
+  .then(data => {
+    res.innerHTML += `
             <img src="${data.avatar_url}" alt="avatar">
             <h1>Diego_dev</h1>
             <span>Desenvolvedor Backend</span>
         `;
-    })
-    .catch(error => console.error('Error fetching data:', error));
+  })
+  .catch(error => console.error('Error fetching data:', error));
+  
+const dados = [{
+    "id": 1,
+    "nome": "Maratona 2024",
+    "des": "Material de estudos para maratona SBC 2024",
+    "file": "../assets/Maratona de programação 2024.pdf"
+},
+{
+    "id": 2,
+    "nome": "Remote Code Execution(RCE)",
+    "des": "Relatorio de pesquisa sobre RCE",
+    "file": "../assets/Relatorio de pesquisa sobre Remote Code Execution(RCE).pdf"
+}
+];
 
-document.addEventListener('DOMContentLoaded', function () {
-    const toggleMenuButton = document.querySelector('.toggle-menu');
-    const navList = document.querySelector('.nav-list');
+const resPDF = document.getElementById("container");
 
-    toggleMenuButton.addEventListener('click', function () {
-        navList.classList.toggle('active');
-    });
+function abrirPdf(fileUrl) {
+    window.location.href = `pdf-viewer.html?file=${encodeURIComponent(fileUrl)}`;
+}
+
+dados.forEach(data => {
+    resPDF.innerHTML += `
+    <div class="card" onclick="abrirPdf('${data.file}')">
+        <h1>${data.nome}</h1>
+        <p>${data.des}</p>
+    </div>
+    `;
+});
+
+
+document.addEventListener('DOMContentLoaded', function() {
+  const toggleMenuButton = document.querySelector('.toggle-menu');
+  const navList = document.querySelector('.nav-list');
+
+  toggleMenuButton.addEventListener('click', function() {
+    navList.classList.toggle('active');
+  });
 });
