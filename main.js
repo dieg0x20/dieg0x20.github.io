cb1c8542d91138314ec956421a306c70c0d077cb
Diego <diego444x@gmail.com>
2025-03-17 16:30:08 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,37 +1,30 @@
-const dados = [{
-    "id": 1,
-    "nome": "Maratona 2024",
-    "des": "Material de estudos para maratona SBC 2024",
-    "file": "../assets/Maratona de programação 2024.pdf"
-},
+const dados = [
   {
-    "id": 2,
+    "id": 1,
     "nome": "Remote Code Execution(RCE)",
     "des": "Relatorio de pesquisa sobre RCE",
     "file": "../assets/Relatorio de pesquisa sobre Remote Code Execution(RCE).pdf"
-}
+  },
+  {
+    "id": 2,
+    "nome": "Manual de Vim para iniciantes",
+    "file": "/assets/vim_by_diego.pdf"
+  }
 ];
+
 const resPDF = document.getElementById("container");
 
 function abrirPdf(fileUrl) {
   window.location.href = `pdf-viewer.html?file=${encodeURIComponent(fileUrl)}`;
 }
 
-dados.forEach(data => {
-  resPDF.innerHTML += `
-    <div class="cardPaper" onclick="abrirPdf('${data.file}')">
-        <h1>${data.nome}</h1>
-        <p>${data.des}</p>
-    </div>
+if (resPDF) {
+  dados.forEach(data => {
+    resPDF.innerHTML += `
+      <button class="btn1" onclick="abrirPdf('${encodeURIComponent(data.file)}')">
+        ${data.nome}
+      </button>
     `;
-});
-
-
-document.addEventListener('DOMContentLoaded', function() {
-  const toggleMenuButton = document.querySelector('.toggle-menu');
-  const navList = document.querySelector('.nav-list');
-
-  toggleMenuButton.addEventListener('click', function() {
-    navList.classList.toggle('active');
   });
-});
+}
+
